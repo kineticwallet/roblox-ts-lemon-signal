f11bfa8aebe802efb5b86be688d4c2ad71c9de75
kineticwallet <105553638+kineticwallet@users.noreply.github.com>
2024-04-08 15:09:36 +0200

    improve types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,27 @@
-import type { Signal } from "./LemonSignal";
+export declare type SignalParameters<T> = Parameters<
+	T extends unknown[] ? (...args: T) => never : T extends unknown ? (arg: T) => never : () => never
+>;
 
-declare const LemonSignal: Signal.Constructor;
-export = LemonSignal;
+export declare type SignalCallback<T> = (...args: SignalParameters<T>) => unknown;
+export declare type SignalWait<T> = T extends unknown[] ? LuaTuple<T> : T;
+
+export declare class Connection<T> {
+	public readonly Connected: boolean;
+
+	public Disconnect(): void;
+	public Reconnect(): void;
+}
+
+export declare class Signal<T> {
+	public readonly RBXScriptConnection?: RBXScriptConnection;
+
+	public static is: <O extends object>(object: O) => boolean;
+	public static wrap: <T extends Callback>(signal: RBXScriptSignal<T>) => Signal<T>;
+
+	public Connect(fn: SignalCallback<T>): Connection<T>;
+	public Once(fn: SignalCallback<T>): Connection<T>;
+	public Wait(): SignalWait<T>;
+	public Fire(...args: SignalParameters<T>): void;
+	public DisconnectAll(): void;
+	public Destroy(): void;
+}
